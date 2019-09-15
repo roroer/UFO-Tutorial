1f215a6139eee808c3c3a54245861ef133e089d5
roroer <meeproroer@gmail.com>
2019-09-15 19:30:40 -0400

    UFO tutorial

---
+++ UFO tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody2D rb2d;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText ();
+    }
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+    void Update() {
+        if (Input.GetKey("escape")) {
+            Application.Quit();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You Win! Game made by Alex Smith";
+        }
+    }
+}
